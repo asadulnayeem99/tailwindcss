42cd7c4261495e90a839c5ee2d589412c4fde578
Md Asadul Islam Nayeem <asadulnayeem99@gmail.comm>
2024-01-12 17:36:05 +0600

    Just in time mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
+  purge: ["./output/**/*.html"],
   content: ["./output/**/*.html"],
   // darkMode: "class",
   theme: {
